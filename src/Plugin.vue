47beed9dc50785b6e0c1a21733cce3dcf53c16e8
Michael Weterings <mail@michaelweterings.dev>
2023-06-05 17:02:55 +0200

    feat: creates linklist fieldtype

--- src/Plugin.vue
+++ src/Plugin.vue
@@ -1,91 +1,66 @@
 <template>
-  <div class="integration">
-      
-      <div class="ft-checkbox">
-        <input type="checkbox" id="backdrop" name="backdrop" value="backdrop" v-model="backdrop">
-        <label for="backdrop">Backdrop</label>
-      </div>
+  <div class="List">
+    <ol class="List__list uk-margin-bottom-remove">
+      <li
+        v-for="(item, index) in model.items"
+        :key="index"
+        class="List__list-item uk-flex uk-flex-middle"
+      >
+        <input
+          v-model="model.items[index].title"
+          class="uk-form-small uk-width-1-1"
+          :aria-label="`List item ${index}`"
+          placeholder="Title"
+        >
 
-      <button @click="toggleIconDropdown()" class="ft-button">Select</button>
-      <div class="ft-panel" v-if="openIconDropdown">
-        <ul class="ft-grid">
-          <li v-for="icon in icons" :key="icon.value" class="ft-grid__item">
-            <button @click="selectIcon({ name: icon.name, value: icon.value })" :class="`ft-button ft-button--icon ${icon.value === selectedIcon ? 'ft-button--active' : '' }`">
-              <div class="ft-panel__icon-holder">
-                <img :src="`${options.domain}/icons/icon.${icon.value}.svg`" class="ft-panel__icon"/>
-              </div>
-            </button>
-          </li>
-        </ul>
-      </div>
+        <input
+          v-model="model.items[index].slug"
+          class="uk-form-small uk-width-1-1 uk-margin-left"
+          :aria-label="`List item ${index}`"
+          placeholder="Slug"
+        >
+        <a
+          class="assets__item-trash"
+          aria-label="Remove item"
+          @click="removeItem(index)"
+          >
+          <i class="uk-icon-minus-circle"></i>
+        </a>
+      </li>
+    </ol>
 
-      <div class="ft-paragraph" v-if="model.value">
-        Selected: {{ model.name }}
-        <button @click="clearIcon()" class="ft-link">clear</button>
-      </div>
+    <a
+      class="blok__full-btn uk-margin-small-top"
+      @click="addItem"
+    >
+      <i class="uk-icon-plus-circle uk-margin-small-right"/>
+      Add item
+    </a>
   </div>
 </template>
 
 <script>
 export default {
   mixins: [window.Storyblok.plugin],
-  data() {
-    return {
-      openIconDropdown: false,
-      icons: [],
-      backdrop: false
-    }
-  },
-
   methods: {
     initWith() {
       return {
         // needs to be equal to your storyblok plugin name
-        plugin: 'storyblok-icon-fieldtype',
-        icons: [],
-        name: null,
-        value: null,
-        backdrop: false
+        plugin: 'storyblok-linklist-fieldtype',
+        items: [],
       }
     },
-    pluginCreated() {
-      this.getDataFromApi()
+    addItem() {
+      this.model.items.push({
+        title: "",
+        slug: ""
+      });
     },
-    toggleIconDropdown() {
-      this.openIconDropdown = !this.openIconDropdown
-
-      this.getDataFromApi()
+    removeItem(index) {
+      this.model.items = this.model.items.filter((_, i) => i !== index);
     },
-    selectIcon({ name, value }) {
-      if (value === this.selectedIcon) {
-        this.clearIcon()
-
-        return;
-      }
-
-      this.model.name = name
-      this.model.value = value
-
-      this.toggleIconDropdown()
-    },
-    clearIcon() {
-      this.model.name = ''
-      this.model.value = ''
-    },
-    async getDataFromApi() {
-      const icons = await fetch('https://api.storyblok.com/v2/cdn/datasource_entries?datasource=icon&token=ecuaQkqnQS2nuhnRFNnpNgtt', { method: 'GET' })
-      .then(response => response.json())
-      .then(response => {
-        return response.datasource_entries
-      })
-      .catch(err => console.error(err));
-
-      this.icons = icons
-    }
-  },
-  computed: {
-    selectedIcon() {
-      return this.model.value
+    handleize(string) {
+      return string.toLowerCase().replace(/[^\w\u00C0-\u024f]+/g, "-").replace(/^-+|-+$/g, "");
     }
   },
   watch: {
@@ -100,77 +75,11 @@ export default {
 </script>
 
 <style> 
-  .ft-button {
-    background-color: #fff;
-    border: 1px solid #ccc;
-    cursor: pointer;
-    height: 2rem;
-    display: block;
-    padding: 0 0.5rem;
-    width: 100%;
-    border-radius: 0.5rem;
+  .List__list {
+    padding-left: 0;
   }
 
-  .ft-button:hover,
-  .ft-button--active {
-    border-color: #00b3b0;
-  }
-
-  .ft-button--icon {
-    height: auto;
-    padding: 0.5rem;
-  }
-
-  .ft-panel {
-    background-color: #fff;
-    border: 1px solid #ccc;
-    margin-top: 1rem;
-    border-radius: 0.5rem;
-    padding: 0.5rem;
-  }
-
-  .ft-grid {
-    list-style: none;
-    padding: 0;
-    margin: 0;
-    display: grid;
-    grid-template-columns: repeat(5, minmax(0, 1fr));
-    gap: 0.5rem;
-    max-height: 10rem;
-    overflow-y: auto;
-  }
-
-  .ft-panel__icon-holder {
-    position: relative;
-    padding-bottom: 100%;
-  }
-  
-  .ft-panel__icon {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-  }
-
-  .ft-paragraph {
-    margin-top: 1rem;
-  }
-
-  .ft-link {
-    background-color: transparent;
-    border: none;
-    padding: 0;
-    appearance: none;
-    text-decoration: underline;
-    cursor: pointer;
-  }
-
-  .ft-checkbox {
-    margin-bottom: 1rem;
-
-    > label {
-      margin-left: 0.5rem;
-    }
+  .List__list-item + .List__list-item {
+    margin-top: 5px;
   }
 </style>
