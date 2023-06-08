9b7cd93d3ef59d4965a579e16f4dd835b59f50a6
Michael Weterings <mail@michaelweterings.dev>
2023-06-08 16:28:52 +0200

    fix: updates naming

--- src/Plugin.vue
+++ src/Plugin.vue
@@ -1,36 +1,42 @@
 <template>
   <div class="List">
-    <ol class="List__list uk-margin-bottom-remove">
+    <ul class="uk-list">
       <li
-        v-for="(item, index) in model.items"
+        v-for="(__, index) in model.items"
         :key="index"
-        class="List__list-item uk-flex uk-flex-middle"
+        class="uk-margin"
       >
-        <input
-          v-model="model.items[index].title"
-          class="uk-form-small uk-width-1-1"
-          :aria-label="`List item ${index}`"
-          placeholder="Title"
-        >
-
-        <input
-          v-model="model.items[index].slug"
-          class="uk-form-small uk-width-1-1 uk-margin-left"
-          :aria-label="`List item ${index}`"
-          placeholder="Slug"
-        >
-        <a
-          class="assets__item-trash"
-          aria-label="Remove item"
-          @click="removeItem(index)"
+        <div class="uk-margin">
+          <input
+            v-model="model.items[index].title"
+            class="uk-input uk-width-1-1"
+            :aria-label="`List item ${index}`"
+            placeholder="Title"
           >
-          <i class="uk-icon-minus-circle"></i>
-        </a>
+        </div>
+        <div class="uk-margin">
+          <textarea
+            v-model="model.items[index].text"
+            class="uk-textarea uk-width-1-1"
+            :aria-label="`List item ${index}`"
+            placeholder="Description"
+            rows="2"
+          ></textarea>
+        </div>
+        <div>
+          <a
+            class="uk-text-small"
+            aria-label="Remove item"
+            @click="removeItem(index)"
+            >
+            <i class="uk-icon-minus-circle"></i> Remove item
+          </a>
+        </div>
       </li>
-    </ol>
+    </ul>
 
     <a
-      class="blok__full-btn uk-margin-small-top"
+      class="uk-button uk-button-default uk-button-small uk-margin-small-top"
       @click="addItem"
     >
       <i class="uk-icon-plus-circle uk-margin-small-right"/>
@@ -46,14 +52,14 @@ export default {
     initWith() {
       return {
         // needs to be equal to your storyblok plugin name
-        plugin: 'storyblok-linklist-fieldtype',
+        plugin: 'storyblok-list-fieldtype',
         items: [],
       }
     },
     addItem() {
       this.model.items.push({
         title: "",
-        slug: ""
+        text: ""
       });
     },
     removeItem(index) {
